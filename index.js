a09fa2d4423b47b5514214a65336820374e7700b
fionaVg1 <41353276+fionaVg1@users.noreply.github.com>
2020-01-12 16:01:54 +0800

    webpack之javascript的编译

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-let program = require('commander');
-program.version('0.0.0')
-    .option('-t,--type <type>', 'project type')
-    .option('-n,--name <name>', 'project name')
-program.command('create').action(function (name) {
-    console.log(program.name,program.type);
-});
-program.parse(process.argv);
+import './app.ts'
+setTimeout(()=>{
+    console.log('fiona');
+},500);
+new Promise(setTimeout(()=>{
+    console.log('cai');
+}))
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ module.exports = {
     // entry:['./app.js','./app2.js'],
     //这个比较常用
     entry:{
-        app:'./app.js',
+        app:'./index.js',
+        // app:['babel-polyfill','./index.js'],
         // app2:'./app2.js'
     },
     output:{
         //path是一个绝对路径，如果没有path，则path默认为__dirname+dist
-        path:__dirname+'/src/bundle',
+        // path:__dirname+'/src/bundle',
         // filename:'bundle.js'
         //name为entry中的指向key值,hash为默认随机字符串，hash:4为截取前4个
         filename:'[name].[hash:4].js'//app.hkgd.js
@@ -20,15 +21,19 @@ module.exports = {
                 exclude:'/node_modules/',
                 //use 是使用哪个loader来处理对应的文件
                 use:{
-                    loader:'babel-loader',                   
+                    loader:'babel-loader',                                     
                 }
-            }           
+            } ,
+            {
+                test:/\.tsx?$/,          
+                use:'ts-loader',              
+            }          
         ]
     },
     plugins:[
-        new webpack.DefinePlugin({
-            'process.env':require('../config/dev.env')
-        }),
-        new webpack.HotModuleReplacementPlugin()
+        // new webpack.DefinePlugin({
+        //     'process.env':require('../config/dev.env')
+        // }),
+        // new webpack.HotModuleReplacementPlugin()
     ]
 }
